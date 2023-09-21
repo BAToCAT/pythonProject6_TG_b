8272d964235fab38f6ad65dcd2a20d124192d705
BAToCAT <prosto2x2@gmail.com>
2023-09-21 19:23:39 +0300

    Initial commit

---
+++ CryptoBot/extensions.py
@@ -0,0 +1,31 @@
+import requests
+import json
+from config import keys
+
+class APIException(Exception):
+    pass
+
+class CryptoConverter:
+    @staticmethod
+    def get_price(quote: str, base: str, amount: str):
+        if quote == base:
+            raise APIException(f'Невозможно конвертировать {base} в {base}')
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f'Неудалось обработать валюту {base}')
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f'Неудалось обработать валюту {quote}')
+        try:
+            amount = float(amount)
+            # if amount != 1:
+            #     amount = amount * total_base
+            #     return amount
+        except ValueError:
+            raise APIException(f'Неудалось обработать количество {amount}')
+
+        r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
+        total_base = amount * json.loads(r.content)[keys[base]]
+        return total_base
